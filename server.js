5ce948ad3c380855a337f710aa74c75424c14e8b
girard_h <girard_h@etna-alternance.net>
2020-10-07 20:45:20 +0200

    testing update

--- server.js
+++ server.js
@@ -117,6 +117,36 @@ MongoClient.connect(mongodbUrl, { useUnifiedTopology : true }, (err, client) => 
     });
   });
 
+  app.post('/word/:value', (req, res) => {
+    console.log('Object.keys(req.body).length : ', Object.keys(req.body).length)
+    if (!req.body['word'])
+      return res.status(422).json({
+        status : 'error',
+        message : 'Mssing word object in parameters, refer to the documentation at /api-docs',
+        data : req.body
+      });
+    wordsCollection.findOneAndUpdate(
+      { 'value' : req.body['word']['value'] },
+      req.body['replace'] ? {  $set : { 'translations' : req.body['word']['translations'] } } : {  $push : { 'translations' : req.body['word']['translations'] } },
+      req.body['upsert'] ? { upsert : true } : { upsert : false },
+      (err, response) => {
+        if (err) {
+          // TODO : check HTTP code
+          console.log(`/word post (update), ${req.body['replace'] ? 'findOneAndReplace' : 'findOneAndUpdate'} : `, err);
+          return res.status(500).json({
+            satus : 'error',
+            message : 'Error while querying database',
+            data : {}
+          });
+        }
+        res.status(201).json({
+          status : 'success',
+          message : 'Successfully updated one entry',
+          data : response.ops
+        });
+    });
+  });
+
   app.delete('/word/:value', (req, res) => {
     // TODO : CHECK PARAMS
     wordsCollection.deleteOne({ value : req.params.value }, (err, response) => {
@@ -143,36 +173,6 @@ MongoClient.connect(mongodbUrl, { useUnifiedTopology : true }, (err, client) => 
         message : 'Successfully deleted one entry',
         data : { value : req.params.value }
       });
-    });
-  });
-
-  app.post('/word/:value', (req, res) => {
-    console.log('Object.keys(req.body).length : ', Object.keys(req.body).length)
-    if (!req.body['word'])
-      return res.status(422).json({
-        status : 'error',
-        message : 'Mssing word object in parameters, refer to the documentation at /api-docs',
-        data : req.body
-      });
-    req.body['replace'] ? wordsCollection.findOneAndReplace : wordsCollection.findOneAndUpdate (
-      {'value' : req.body['word']['value'] },
-      req.body['world'],
-      req.body['upsert'] ? { upsert : true } : { upsert : false },
-      (err, response) => {
-        if (err) {
-          // TODO : check HTTP code
-          console.log(`/word post (update), ${req.body['replace'] ? 'findOneAndReplace' : 'findOneAndUpdate'} : `, err);
-          return res.status(500).json({
-            satus : 'error',
-            message : 'Error while querying database',
-            data : {}
-          });
-        }
-        res.status(201).json({
-          status : 'success',
-          message : 'Successfully updated one entry',
-          data : response.ops
-        });
     });
   });
 
